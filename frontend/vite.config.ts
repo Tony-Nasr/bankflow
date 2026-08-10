ea98e037e5413bdf92058e200cf946b296c1c925
Tony Nasr <naser.naser.201999@gmail.com>
2026-08-10 15:07:11 +0300

    set Render backend URL for production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
@@ -13,4 +13,11 @@ export default defineConfig({
       "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
-})
+
+  //new
+  define: {
+    __API_URL__: mode === 'production'
+      ? '"https://bankflow-4u21.onrender.com/api"'
+      : '"http://localhost:5004/api"'
+  }
+}))
